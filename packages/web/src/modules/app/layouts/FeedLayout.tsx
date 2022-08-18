ebacc3782ccee8105754325cd56223d3eabd3f40
Sreekar Venkata Nutulapati <sreekarnv1@gmail.com>
2022-08-18 20:54:59 +0530

    responsive ui for feed layout

--- packages/web/src/modules/app/layouts/FeedLayout.tsx
+++ packages/web/src/modules/app/layouts/FeedLayout.tsx
@@ -2,21 +2,22 @@ import { ChevronDownIcon } from '@chakra-ui/icons';
 import {
 	Avatar,
 	Box,
-	Button,
 	Flex,
 	HStack,
 	Text,
-	Menu,
-	MenuButton,
-	MenuList,
-	MenuItem,
-	MenuDivider,
 	useDisclosure,
 	Grid,
 	GridItem,
+	Menu,
+	Button,
+	MenuButton,
+	MenuItem,
+	MenuList,
+	MenuDivider,
 } from '@chakra-ui/react';
 import { useQueryClient } from '@tanstack/react-query';
 import { Link } from 'gatsby';
+
 import React from 'react';
 import Logo from '../../shared/components/Logo';
 import ThemeToggler from '../../shared/components/ThemeToggler';
@@ -46,7 +47,11 @@ const FeedLayout: React.FC<FeedLayoutProps> = ({ children }) => {
 				<HStack>
 					<Logo />
 				</HStack>
-				<HStack as='ul' listStyleType={'none'} spacing={8}>
+				<HStack
+					display={{ base: 'none', md: 'flex' }}
+					as='ul'
+					listStyleType={'none'}
+					spacing={8}>
 					<HStack>
 						<Avatar src={user.photo?.url} name={user.name} />
 						<Text fontWeight='semibold'>{user.name}</Text>
@@ -56,22 +61,91 @@ const FeedLayout: React.FC<FeedLayoutProps> = ({ children }) => {
 						<ThemeToggler />
 					</Box>
 				</HStack>
+				<Box display={{ base: 'block', md: 'none' }}>
+					<Menu>
+						<MenuButton
+							as={Button}
+							variant='ghost'
+							rightIcon={<ChevronDownIcon />}
+							_hover={{
+								bgColor: 'transparent',
+							}}
+							_active={{
+								bgColor: 'transparent',
+							}}>
+							<Avatar src={user.photo?.url} name={user.name} />
+						</MenuButton>
+						<MenuList>
+							<MenuItem as={Link} to='/app/feed' fontWeight='semibold'>
+								My Feed
+							</MenuItem>
+							<MenuItem as={Link} to='/app/posts/new' fontWeight='semibold'>
+								Create Post
+							</MenuItem>
+							<MenuDivider />
+							<MenuItem as={Link} to='/app/profile' fontWeight='semibold'>
+								My Profile
+							</MenuItem>
+							<MenuItem
+								as={Link}
+								to='/app/profile/update'
+								fontWeight='semibold'>
+								Update Profile
+							</MenuItem>
+							<MenuDivider />
+							<MenuItem
+								as={Link}
+								to='/auth/logout'
+								fontWeight='semibold'
+								color='red.400'>
+								Logout
+							</MenuItem>
+						</MenuList>
+					</Menu>
+				</Box>
 			</Flex>
 
 			<Grid
-				templateColumns={
-					'1.2rem [content-start] repeat(12, [col-start] 1fr [col-end]) [content-end] 1.2rem'
-				}>
-				<GridItem gridColumn={'content-start / col-end 2'}>
+				templateColumns={{
+					base: '1rem [content-start] 1fr [content-end] 1rem',
+					lg: '.01rem [content-start] repeat(12, [col-start] 1fr [col-end]) [content-end] .01rem',
+					'2xl':
+						'1.2rem [content-start] repeat(12, [col-start] 1fr [col-end]) [content-end] 1.2rem',
+				}}
+				columnGap={{ lg: 4, xl: 6, '2xl': 0 }}>
+				<GridItem
+					display={{
+						base: 'none',
+						lg: 'block',
+					}}
+					gridColumn={{
+						'2xl': 'content-start / col-end 2',
+						xl: 'content-start / col-end 3',
+						lg: 'content-start / col-end 4',
+					}}>
 					<Box position={'sticky'} zIndex='sticky' top='24'>
 						<FeedLinkItem color='purple' name='My Feed' to='/app/feed' />
 						<FeedLinkItem name='Create Post' to='/app/posts/new' />
 					</Box>
 				</GridItem>
-				<GridItem gridColumn={'col-start 4 / col-end 9'}>
-					<Box pt='7'>{children}</Box>
+				<GridItem
+					gridColumn={{
+						base: 'content-start / content-end',
+						lg: 'col-start 5 / content-end',
+						xl: 'col-start 4 / col-end 9',
+					}}>
+					<Box pt={{ base: '5', xl: '7' }}>{children}</Box>
 				</GridItem>
-				<GridItem py='4' gridColumn={'col-start 11 / content-end'}>
+				<GridItem
+					py='4'
+					display={{
+						base: 'none',
+						xl: 'block',
+					}}
+					gridColumn={{
+						'2xl': 'col-start 11 / content-end',
+						xl: 'col-start 10 / content-end',
+					}}>
 					<Box width='100%' position={'sticky'} zIndex='sticky' top='24'>
 						<UserProfileCard />
 					</Box>
